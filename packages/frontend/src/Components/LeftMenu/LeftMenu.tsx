804f836475c5ca1dc4eeb7922b214c79b3158733
Jonics7 <jonics7@mail.ru>
2021-06-12 15:47:33 +0300

    i think this is well

--- packages/frontend/src/Components/LeftMenu/LeftMenu.tsx
+++ packages/frontend/src/Components/LeftMenu/LeftMenu.tsx
@@ -17,7 +17,16 @@ const LeftMenu: React.FunctionComponent = () => {
                 <Animal />
             </div>
 
-            <div className="LeftMenu-policy">by continuing you agree with terms of use and Warible privacy policy</div>
+            <div className="LeftMenu-policy">
+                by continuing you agree with terms of use
+                <br />
+                and Warible privacy policy
+            </div>
+
+            <div className="LeftMenu-footer">
+                <div className="version">v0.1</div>
+                <div className="course">1 ETH ⇆ 2327.61 USD</div>
+            </div>
         </div>
     );
 };
