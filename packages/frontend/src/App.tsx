af5a666ea6999c0a7d17a390aa6757d577330796
Jonics7 <jonics7@mail.ru>
2021-06-12 14:14:57 +0300

    fix shit

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+    return (
+        <div className="App">
+            <video src="https://youtu.be/9kpv-xtFifk"></video>
+            <div className="test"></div>
+        </div>
+    );
+};
+
+export default App;
