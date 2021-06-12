059a6bc51827e8101e9cb8ffd95f63d2c83b437d
Jonics7 <jonics7@mail.ru>
2021-06-12 17:23:28 +0300

    made login page (without metamask)

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import PageWrapper from './Components/PageWrapper/PageWrapper';
+import GameMenu from './Pages/GameMenu/GameMenu';
 import StartPage from './Pages/StartPage/StartPage';
 
 const App: React.FunctionComponent = () => {
     return (
         <PageWrapper>
             <Switch>
-                <Route exact path="/" component={StartPage} />
+                <Route exact path="/" component={GameMenu} />
+                <Route exact path="/login" component={StartPage} />
             </Switch>
         </PageWrapper>
     );
--- packages/frontend/src/Components/LeftMenu/LeftMenu.tsx
+++ packages/frontend/src/Components/LeftMenu/LeftMenu.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LeftMenu.scss';
 import { ReactComponent as Logo } from '../../Assets/Svg/logo.svg';
 import { ReactComponent as Animal } from '../../Assets/Svg/animal.svg';
+import { ReactComponent as AnimalDark } from '../../Assets/Svg/animal-dark.svg';
+import { useEffect } from 'react';
+import MetaMaskOnboarding from '@metamask/onboarding';
+import { useHistory } from 'react-router-dom';
 
 const LeftMenu: React.FunctionComponent = () => {
+    const [time, setTime] = useState<string>();
+    const [active, setActive] = useState<boolean>(false);
+    const [accounts, setAccounts] = React.useState([]);
+    const onboarding = React.useRef<MetaMaskOnboarding>();
+
+    const history = useHistory();
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(new Date().toLocaleString());
+        }, 1000);
+    }, []);
+
+    // .useEffect(() => {
+    //     if (!onboarding.current) {
+    //       onboarding.current = new MetaMaskOnboarding();
+    //     }
+    //   }, []);
+
+    const OnClick = () => {
+        history.push('/');
+    };
+
     return (
         <div className="LeftMenu-container">
             <div className="LeftMenu-title">Welcome to Warible</div>
-            <div className="LeftMenu-time">12:43:10</div>
+            <div className="LeftMenu-time">{time?.substr(12, 999)}</div>
             <div className="LeftMenu-logo">
                 <Logo />
             </div>
 
-            <div className="LeftMenu-button">
+            <div
+                className={`LeftMenu-button ${active ? 'active' : ''}`}
+                onPointerDown={() => setActive(true)}
+                onPointerUp={() => setActive(false)}
+            >
                 <div className="text">connect metamask</div>
-                <Animal />
+                {active ? <Animal /> : <AnimalDark />}
             </div>
 
             <div className="LeftMenu-policy">
