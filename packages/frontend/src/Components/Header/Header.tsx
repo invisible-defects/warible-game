b0e2ffda16530402f0db10e1531ccf9d7742727b
Jonics7 <jonics7@mail.ru>
2021-06-12 20:12:01 +0300

    idk

--- packages/frontend/src/Components/Header/Header.tsx
+++ packages/frontend/src/Components/Header/Header.tsx
@@ -5,10 +5,10 @@ import './Header.scss';
 const Header: React.FunctionComponent = () => {
     return (
         <div className="Header">
-            <HeaderButton />
-            <HeaderButton />
-            <HeaderButton />
-            <HeaderButton />
+            <HeaderButton active type="Home" />
+            <HeaderButton active={false} type="Deck" />
+            <HeaderButton active={false} type="Inventary" />
+            <HeaderButton active={false} type="Fight" />
         </div>
     );
 };
--- packages/frontend/src/Components/HeaderButton/HeaderButton.tsx
+++ packages/frontend/src/Components/HeaderButton/HeaderButton.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import './HeaderButton.scss';
+import { ReactComponent as Home } from '../../Assets/Svg/logo.svg';
+import { ReactComponent as Deck } from '../../Assets/Svg/logo.svg';
+import { ReactComponent as Inventary } from '../../Assets/Svg/logo.svg';
+import { ReactComponent as Fight } from '../../Assets/Svg/logo.svg';
+
+type CardType = 'Home' | 'Deck' | 'Inventary' | 'Fight';
 
 interface HeaderButtonProps {
     active: boolean;
+    type: CardType;
 }
 
-const HeaderButton: React.FunctionComponent = () => {
-    return <div className="HeaderButton"></div>;
+const GetCardIcon = (cardType: CardType) => {
+    switch (cardType) {
+        case 'Home':
+            return <Home />;
+
+        case 'Deck':
+            return <Deck />;
+
+        case 'Inventary':
+            return <Inventary />;
+
+        case 'Fight':
+            return <Fight />;
+    }
+};
+
+const HeaderButton: React.FunctionComponent<HeaderButtonProps> = ({ active, type }) => {
+    return (
+        <div className={`HeaderButton ${active ? 'active' : ''}`}>
+            <div className="HeaderButton-icon">{GetCardIcon(type)}</div>
+        </div>
+    );
 };
 
 export default HeaderButton;
