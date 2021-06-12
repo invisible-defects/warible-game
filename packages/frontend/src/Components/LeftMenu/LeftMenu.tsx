4d17a61131d99774d9dd7dc48b62153168333a80
Jonics7 <jonics7@mail.ru>
2021-06-12 15:32:37 +0300

    add come elements to start page

--- packages/frontend/src/Components/LeftMenu/LeftMenu.tsx
+++ packages/frontend/src/Components/LeftMenu/LeftMenu.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './LeftMenu.scss';
+import { ReactComponent as Logo } from '../../Assets/Svg/logo.svg';
+import { ReactComponent as Animal } from '../../Assets/Svg/animal.svg';
 
 const LeftMenu: React.FunctionComponent = () => {
     return (
         <div className="LeftMenu-container">
-            <div className="LeftMenu-title">Welcom to Warible</div>
+            <div className="LeftMenu-title">Welcome to Warible</div>
+            <div className="LeftMenu-time">12:43:10</div>
+            <div className="LeftMenu-logo">
+                <Logo />
+            </div>
+
+            <div className="LeftMenu-button">
+                <div className="text">connect metamask</div>
+                <Animal />
+            </div>
+
+            <div className="LeftMenu-policy">by continuing you agree with terms of use and Warible privacy policy</div>
         </div>
     );
 };
