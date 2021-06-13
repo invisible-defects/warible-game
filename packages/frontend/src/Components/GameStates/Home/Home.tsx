13e1616da9df156f9b21edb9bb258782c78288a1
Jonics7 <jonics7@mail.ru>
2021-06-13 13:05:12 +0300

    made home page

--- packages/frontend/src/Components/GameStates/Home/Home.tsx
+++ packages/frontend/src/Components/GameStates/Home/Home.tsx
@@ -11,15 +11,33 @@ const Home: React.FunctionComponent = () => {
                 <div className="Home-icon">
                     <Cards />
                 </div>
+                <div className="Home-bottom">
+                    <div className="Home-title">Collection</div>
+                    <div className="Home-description">
+                        Scroll through your card collection and build a deck to destroy your enemies with
+                    </div>
+                </div>
             </div>
             <div className="Home-container">
                 <div className="Home-icon">
                     <Chest />
                 </div>
+                <div className="Home-bottom">
+                    <div className="Home-title">Lootboxes</div>
+                    <div className="Home-description">
+                        Open smart-contract based lootboxes to obtain powerful fighters for your deck
+                    </div>
+                </div>
             </div>
             <div className="Home-container">
-                <div className="Home-logo">
+                <div className="Home-icon">
                     <Swords />
+                </div>
+                <div className="Home-bottom">
+                    <div className="Home-title">Matchmaking</div>
+                    <div className="Home-description">
+                        Test your skills and decks against other players in a tough 1v1 match
+                    </div>
                 </div>
             </div>
         </div>
