a3cb8e4086cd7ff124d91d090d6d1bafa8070f0a
Jonics7 <jonics7@mail.ru>
2021-06-13 11:08:26 +0300

    start collections

---
+++ packages/frontend/src/Components/GameStates/Collections/CollectionsHeaderItem.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CollectionsHeaderItem: React.FunctionComponent = () => {
+    return null;
+};
+
+export default CollectionsHeaderItem;
--- packages/frontend/src/Pages/GameMenu/Game.tsx
+++ packages/frontend/src/Pages/GameMenu/Game.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import Collections from '../../Components/GameStates/Collections/Collections';
 // import GameView from '../../Components/Game/GameView';
 import Home from '../../Components/GameStates/Home/Home';
 import Header from '../../Components/Header/Header';
@@ -13,12 +14,22 @@ const Game: React.FunctionComponent = () => {
         setCurrentState(type);
     };
 
+    const GetCurrentComponent = (type: CardType) => {
+        switch (type) {
+            case 'Home':
+                return <Home />;
+
+            case 'Deck':
+                return <Collections />;
+        }
+    };
+
     return (
         <div className="GamePage">
             <Header setState={SetState} currentState={currentState} />
             <div className="GameWrapper">
                 {/* <GameView /> */}
-                <Home />
+                {GetCurrentComponent(currentState)}
             </div>
         </div>
     );
